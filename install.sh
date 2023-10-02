f933f9cea30a797abf99b22ffbb288ef52aff376
rclovis <clovis.rabot@epitech.eu>
2023-10-02 14:25:18 +0200

    🌟 Lets go

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 if [ -d "$HOME/.oh-my-zsh" ]; then
     echo "oh-my-zsh is already installed"
 else
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+echo "omzz installed"
 script_dir=$(dirname "$(readlink -f "$0")")
 
 cp -r "$script_dir" ~/.local/share/omzz
 
-touch .zprofile
 echo "source ~/.local/share/omzz/main.sh" >> ~/.zshenv
-
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 rm -rf "$script_dir"
