7f7af16098a62e14f01068966e94f360bd759b0e
rclovis <clovis.rabot@epitech.eu>
2023-10-02 10:20:31 +0200

    🌟 Lets go

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+if [ -d "$HOME/.oh-my-zsh" ]; then
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 script_dir=$(dirname "$(readlink -f "$0")")
 
-cp -r script_dir ~/.local/share/omzz
+cp -r "$script_dir" ~/.local/share/omzz
 
 touch .zprofile
 echo "source ~/.local/share/omzz/main.sh" >> ~/.zprofile
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-rm -rf script_dir
+rm -rf "$script_dir"
